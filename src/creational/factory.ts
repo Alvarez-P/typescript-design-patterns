4f6e68f01134af5d650c2d27631cca1bbd81aeae
Esteban Alvarez <alvarez.p.esteban@gmail.com>
2025-07-04 12:27:46 -0500

    feat(creational): factory

---
+++ src/creational/factory.ts
@@ -0,0 +1,54 @@
+export interface Logger {
+	log(message: string): void
+	error(message: string): void
+}
+
+export class ConsoleLogger implements Logger {
+	constructor(private target: string) {}
+
+	log(message: string): void {
+		console.log(`LOG: [${this.target}] ${message}`)
+	}
+
+	error(message: string): void {
+		console.log(`ERROR: [${this.target}] ${message}`)
+	}
+}
+
+export class FileLogger implements Logger {
+	constructor(private target: string) {}
+
+	log(message: string): void {
+		console.log(`LOG: [${this.target}] ${message}`)
+	}
+
+	error(message: string): void {
+		console.log(`ERROR: [${this.target}] ${message}`)
+	}
+}
+
+export class DatadogLogger implements Logger {
+	constructor(private target: string) {}
+
+	log(message: string): void {
+		console.log(`LOG: [${this.target}] ${message}`)
+	}
+
+	error(message: string): void {
+		console.log(`ERROR: [${this.target}] ${message}`)
+	}
+}
+
+export class LoggerFactory {
+	private readonly loggers = {
+		console: ConsoleLogger,
+		file: FileLogger,
+		external: DatadogLogger
+	}
+
+	createLogger(type: keyof typeof this.loggers, target: string): Logger {
+		const LoggerClass = this.loggers[type]
+		if (!LoggerClass) throw new Error(`Logger type "${type}" is not supported.`)
+		return new LoggerClass(target)
+	}
+}
